e07455d93ae416ad47bd372476b9cb8f8eebee83
Jerome Eteve <jerome@broadbean.com>
2016-07-16 15:06:05 +0100

    First test

---
+++ cpanfile
@@ -0,0 +1,7 @@
+#-*- mode: Perl -*-
+# vim: set syntax=perl:
+
+requires 'Moose' , '>= 2.1801';
+
+test_requires 'Test::Most', '>= 0.34';
+test_requires 'Test::Pod::Coverage', '>= 1.08';
--- lib/App/JESP.pm
+++ lib/App/JESP.pm
@@ -1,5 +1,34 @@
-use strict;
-use warnings;
 package App::JESP;
+
+use Moose;
+
+=head1 NAME
+
+App::JESP - Just Enough SQL Patches
+
+=cut
+
+=head1 SYNOPSIS
+
+Use the command line utility:
+
+  jesp
+
+Or use from your own program:
+
+  my $jesp = App::JESP->new({ ... });
+
+=cut
+
+=head1 DEVELOPMENT
+
+=for html <a href="https://travis-ci.org/jeteve/App-JESP"><img src="https://travis-ci.org/jeteve/App-JESP.svg?branch=master"></a>
+
+=head1 COPYRIGHT
+
+This software is released under the Artistic Licence by Jerome Eteve. Copyright 2016.
+A copy of this licence is enclosed in this package.
+
+=cut
 
 1;
